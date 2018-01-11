00936f66b01c221a09e5faeb050dd468e1abf23a
Robbie Page <rob@rorpage.com>
2018-01-11 10:06:24 -0500

    cleanup script

--- gdq.sh
+++ gdq.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 game=$(curl -s https://api.twitch.tv/kraken/channels/gamesdonequick -H 'client-id:jzkbprff40iqj646a697cyrvl0zt2m6' | /usr/local/bin/jq -r '.game')
+donations=$(curl -s https://gamesdonequick.com/tracker/donations/agdq2018 | /usr/local/bin/pup 'h2.text-center small text{}')
+
 echo "$game | color=purple"
 echo "---"
 echo "Games Done Quick | href=https://gamesdonequick.com"
 echo "---"
-donations=$(curl -s https://gamesdonequick.com/tracker/donations/agdq2018 | /usr/local/bin/pup 'h2.text-center small text{}')
 echo $donations
 exit 0
